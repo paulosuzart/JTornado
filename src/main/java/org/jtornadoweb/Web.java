94f236c8e24905c95df705abeaf8030080aefb78
rafaelfelini <felini@Rafael-Felinis-MacBook-Pro.local>
2010-07-15 23:42:41 -0300

    Added some variables to RequestHandler

--- src/main/java/org/jtornadoweb/Web.java
+++ src/main/java/org/jtornadoweb/Web.java
@@ -10,22 +10,115 @@ import java.util.regex.Pattern;
 
 import org.jtornadoweb.HttpServer.HttpRequest;
 
+/**
+ * The JTornado web framework.
+ * 
+ * @author rafael.felini
+ */
 public class Web {
 
+	/**
+	 * If you want to define a handler for one of the SUPPORTED_METHODS subclass
+	 * this class.
+	 * 
+	 * 
+	 * @author rafael.felini
+	 */
 	public static class RequestHandler {
 
-		public HttpRequest request;
+		/**
+		 * Override the class variable SUPPORTED_METHODS to support more or less
+		 * methods.
+		 */
+		private static final String[] SUPPORTED_METHODS = { "GET", "HEAD",
+				"POST", "DELETE", "PUT" };
 
+		private Application application;
+		
+		private HttpRequest request;
+		
+		private boolean headersWritten;
+
+		private boolean finished;
+		
+		private boolean autoFinish;
+
+		//TODO change this type
 		private String writeBuffer;
+		
+		//TODO private Transform transforms; to be implemented
+		//TODO ui
+		//TODO ui["modules"]
+		
+		private int statusCode;
+		
+		private Map<String, String> headers; //_headers in web.py
 
-		private boolean finished = false;
+		public RequestHandler(Application application, HttpRequest request) {
+			this.application = application;
+			this.request = request;
+			this.autoFinish = true;
+			this.headers = new HashMap<String, String>();
+			
+			this.clear();
+		}
+
+		protected void head() {
+			throw new HttpError(405);
+		}
 
 		protected void get() {
-			// TODO throw HTTPError 500
+			throw new HttpError(405);
 		}
 
 		protected void post() {
-			// TODO throw HTTPError 500
+			throw new HttpError(405);
+		}
+
+		protected void delete() {
+			throw new HttpError(405);
+		}
+
+		protected void put() {
+			throw new HttpError(405);
+		}
+
+		/**
+		 * Called before the actual handler method.
+		 * 
+		 * Useful to override in a handler if you want a common bottleneck for
+		 * all of your request
+		 */
+		protected void prepare() {
+		}
+		
+		/**
+		 * Resets all headers and content for this response
+		 */
+		private void clear() {
+			headers.put("Server", "JTornadoServer/0.1");
+			headers.put("Content-Type", "text/html; charset=UTF-8");
+			
+			/* TODO implement supportsHttp1_1 on HttpRequest
+			if (request.supportsHttp_1_1) {
+				if (request.headers.get("Connection", "").equals("Keep-Alive")) {
+					setHeader("Connection", "Keep-Alive");
+				}
+			}
+			*/
+			
+			writeBuffer = "";
+			statusCode = 200;
+		}
+		
+		/**
+		 * Set the status of a response.
+		 * 
+		 * @param statusCode
+		 */
+		protected final void setStatus(int statusCode) {
+			assert HttpCode.codes.keySet().contains(statusCode);
+			this.statusCode = statusCode;
 		}
 
 		/**
@@ -165,97 +258,107 @@ public class Web {
 		}
 
 	}
-	
+
 	public static class HttpCode {
-		
+
 		public static final Map<Integer, String> codes = new HashMap<Integer, String>();
-		
+
 		static {
-		    codes.put(100, "Continue");
-		    codes.put(101, "Switching Protocols");
+			codes.put(100, "Continue");
+			codes.put(101, "Switching Protocols");
 
-		    codes.put(200, "OK");
-		    codes.put(201, "Created");
-		    codes.put(202, "Accepted");
-		    codes.put(203, "Non-Authoritative Information");
-		    codes.put(204, "No Content");
-		    codes.put(205, "Reset Content");
-		    codes.put(206, "Partial Content");
+			codes.put(200, "OK");
+			codes.put(201, "Created");
+			codes.put(202, "Accepted");
+			codes.put(203, "Non-Authoritative Information");
+			codes.put(204, "No Content");
+			codes.put(205, "Reset Content");
+			codes.put(206, "Partial Content");
 
-		    codes.put(300, "Multiple Choices");
-		    codes.put(301, "Moved Permanently");
-		    codes.put(302, "Found");
-		    codes.put(303, "See Other");
-		    codes.put(304, "Not Modified");
-		    codes.put(305, "Use Proxy");
-		    codes.put(306, "(Unused);");
-		    codes.put(307, "Temporary Redirect");
+			codes.put(300, "Multiple Choices");
+			codes.put(301, "Moved Permanently");
+			codes.put(302, "Found");
+			codes.put(303, "See Other");
+			codes.put(304, "Not Modified");
+			codes.put(305, "Use Proxy");
+			codes.put(306, "(Unused);");
+			codes.put(307, "Temporary Redirect");
 
-		    codes.put(400, "Bad Request");
-		    codes.put(401, "Unauthorized");
-		    codes.put(402, "Payment Required");
-		    codes.put(403, "Forbidden");
-		    codes.put(404, "Not Found");
-		    codes.put(405, "Method Not Allowed");
-		    codes.put(406, "Not Acceptable");
-		    codes.put(407, "Proxy Authentication Required");
-		    codes.put(408, "Request Timeout");
-		    codes.put(409, "Conflict");
-		    codes.put(410, "Gone");
-		    codes.put(411, "Length Required");
-		    codes.put(412, "Precondition Failed");
-		    codes.put(413, "Request Entity Too Large");
-		    codes.put(414, "Request-URI Too Long");
-		    codes.put(415, "Unsupported Media Type");
-		    codes.put(416, "Requested Range Not Satisfiable");
-		    codes.put(417, "Expectation Failed");
+			codes.put(400, "Bad Request");
+			codes.put(401, "Unauthorized");
+			codes.put(402, "Payment Required");
+			codes.put(403, "Forbidden");
+			codes.put(404, "Not Found");
+			codes.put(405, "Method Not Allowed");
+			codes.put(406, "Not Acceptable");
+			codes.put(407, "Proxy Authentication Required");
+			codes.put(408, "Request Timeout");
+			codes.put(409, "Conflict");
+			codes.put(410, "Gone");
+			codes.put(411, "Length Required");
+			codes.put(412, "Precondition Failed");
+			codes.put(413, "Request Entity Too Large");
+			codes.put(414, "Request-URI Too Long");
+			codes.put(415, "Unsupported Media Type");
+			codes.put(416, "Requested Range Not Satisfiable");
+			codes.put(417, "Expectation Failed");
 
-		    codes.put(500, "Internal Server Error");
-		    codes.put(501, "Not Implemented");
-		    codes.put(502, "Bad Gateway");
-		    codes.put(503, "Service Unavailable");
-		    codes.put(504, "Gateway Timeout");
-		    codes.put(505, "HTTP Version Not Supported");
+			codes.put(500, "Internal Server Error");
+			codes.put(501, "Not Implemented");
+			codes.put(502, "Bad Gateway");
+			codes.put(503, "Service Unavailable");
+			codes.put(504, "Gateway Timeout");
+			codes.put(505, "HTTP Version Not Supported");
 
 		}
-		
+
 		public static String get(int code) {
 			return codes.get(code);
 		}
-		
+
 	}
-	
+
 	/**
 	 * An exception that will turn into an HTTP error response.
 	 * 
 	 * @author rafael.felini@gmail.com
 	 */
-	public static class HttpError {
-		
+	public static class HttpError extends RuntimeException {
+
+		private static final long serialVersionUID = 1L;
+
 		private final int statusCode;
 		private final String logMessage;
 		private final String[] args;
 		private final static Formatter formatter = new Formatter();
-		
-		public HttpError (int statusCode, String logMessage, String... args) {
+
+		public HttpError(int statusCode, String... args) {
+			this.statusCode = statusCode;
+			this.logMessage = null;
+			this.args = args;
+		}
+
+		public HttpError(int statusCode, String logMessage, String... args) {
 			this.statusCode = statusCode;
 			this.logMessage = logMessage;
 			this.args = args;
 		}
-		
+
 		@Override
 		public String toString() {
-			
-			String message = formatter.format("HTTP %d: %s ", statusCode, HttpCode.get(statusCode)).toString();
-			
+
+			String message = formatter.format("HTTP %d: %s ", statusCode,
+					HttpCode.get(statusCode)).toString();
+
 			if (logMessage != null && !logMessage.isEmpty()) {
-				return message + "(" + logMessage + " " + Arrays.toString(args) + ")";
+				return message + "(" + logMessage + " " + Arrays.toString(args)
+						+ ")";
 			} else {
 				return message;
 			}
 
 		}
-		
+
 	}
 
 }
